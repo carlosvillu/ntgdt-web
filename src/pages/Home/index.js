baf66a6d287eacd82aec1d822363f92982432ae4
Carlos Villuendas <carlosvillu@gmail.com>
2018-12-12 23:27:17 +0100

    feat(pages): lazy load Grid while retrive info from DB

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import {hot} from 'react-hot-loader'
 
-const Grid = React.lazy(() => import('../../components/Grid'))
+import {useFirebaseRef} from '../../hooks/firebase'
 
-const Home = () => (
-  <div className="Home">
-    <Grid />
-  </div>
-)
+const GridPromise = import(/* webpackChunkName: 'Grid' */ '../../components/Grid')
+const Grid = React.lazy(() => GridPromise)
+
+const Home = () => {
+  const {loading, items = {}} = useFirebaseRef('/entries')
+  return (
+    <React.Suspense fallback={<p>Loading suspense GRID</p>}>
+      <div className="Home">
+        {!loading ? <Grid items={items} /> : <h1>Loading database</h1>}
+      </div>
+      <React.Suspense fallback={null}>
+        <div hidden>
+          <Grid items={items} />
+        </div>
+      </React.Suspense>
+    </React.Suspense>
+  )
+}
 
 Home.displayName = 'Home'
 
-export default hot(module)(Home)
+export default Home
