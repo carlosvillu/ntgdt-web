936753db2b08425eded48989e98bce93cf89c626
Carlos Villuendas <carlosvillu@gmail.com>
2018-12-12 18:52:57 +0100

    feat(components): item now has icons at the bottom

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -8,15 +8,13 @@ const Send = React.lazy(() => import('../Icons/Send'))
 
 const Item = ({urlImage, alt}) => (
   <div className="Item">
-    <div className="Item-toolbar">
-      <h2 className="Item-title">{alt}</h2>
-      <div className="Item-icons">
-        <Favorite className="Item-icon" isFavorite />
-        <GetApp className="Item-icon" />
-        <Send className="Item-icon" />
-      </div>
-    </div>
+    <h2 className="Item-title">{alt}</h2>
     <Image src={urlImage} alt={alt} />
+    <div className="Item-icons">
+      <Favorite className="Item-icon" isFavorite />
+      <GetApp className="Item-icon" />
+      <Send className="Item-icon" />
+    </div>
   </div>
 )
 
