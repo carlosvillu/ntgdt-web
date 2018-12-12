181514d02c1558cb03b633e9105ac7735275bb45
Carlos Villuendas <carlosvillu@gmail.com>
2018-12-12 23:24:47 +0100

    feat(Root): sW for assets

--- src/app.js
+++ src/app.js
@@ -5,9 +5,16 @@ import Router from 'react-router/lib/Router'
 import browserHistory from 'react-router/lib/browserHistory'
 import routes from './routes'
 
-import('./styles/index.scss')
+import {register} from '@s-ui/bundler/registerServiceWorker'
+
+import(/* webpackChunkName: 'index.scss' */ './styles/index.scss')
 
 ReactDOM.render(
   <Router routes={routes} history={browserHistory} />,
   document.getElementById('⚛️')
 )
+
+register({
+  first: () => window.alert('Content is cached for offline use.'),
+  renovate: () => window.alert('New content is available; please refresh.')
+})()
