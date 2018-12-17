09b864edf9289455b0080424186a530b3d99b14d
Carlos Villuendas <carlosvillu@gmail.com>
2018-12-17 19:55:40 +0100

    feat(pages): remove Suspense from Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,22 +2,17 @@ import React from 'react'
 
 import {useFirebaseRef} from '../../hooks/firebase'
 
-const GridPromise = import(/* webpackChunkName: 'Grid' */ '../../components/Grid')
-const Grid = React.lazy(() => GridPromise)
+// const GridPromise = import(/* webpackChunkName: 'Grid' */ '../../components/Grid')
+// const Grid = React.lazy(() => GridPromise)
+
+import Grid from '../../components/Grid'
 
 const Home = () => {
-  const {loading, items = {}} = useFirebaseRef('/entries')
+  const {loading, items = []} = useFirebaseRef('/entries')
   return (
-    <React.Suspense fallback={<p>Loading suspense GRID</p>}>
-      <div className="Home">
-        {!loading ? <Grid items={items} /> : <h1>Loading database</h1>}
-      </div>
-      <React.Suspense fallback={null}>
-        <div hidden>
-          <Grid items={items} />
-        </div>
-      </React.Suspense>
-    </React.Suspense>
+    <div className="Home">
+      {!loading ? <Grid items={items} /> : <h1>Loading database</h1>}
+    </div>
   )
 }
 
