721263537f862e052686ac8f9aa203d36267ed0d
Carlos Villuendas <carlosvillu@gmail.com>
2018-12-17 19:56:53 +0100

    feat(hooks): create lazy load image hook

---
+++ src/hooks/images.js
@@ -0,0 +1,43 @@
+import {useRef, useEffect} from 'react'
+
+const imgWith = url => width =>
+  `https://res.cloudinary.com/carlosvillu/image/fetch/w_${width},f_auto/${url}`
+
+export const useImages = ({blurImg, fullImage}) => {
+  const imgRef = useRef(null)
+
+  useEffect(
+    () => {
+      imgRef.current.src = blurImg
+      imgRef.current.classList.add('is-loading')
+    },
+    [imgRef.current]
+  )
+
+  useEffect(
+    () => {
+      const imgURLWith = imgWith(fullImage)
+      const imgInMemory = new window.Image()
+      const srcSet = `
+    ${imgURLWith(320)} 320w,
+    ${imgURLWith(480)} 480w,
+    ${imgURLWith(768)} 768w,
+    ${imgURLWith(1024)} 1024w,
+    ${imgURLWith(1280)} 1280w`
+      const sizes =
+        '(max-width: 20em) 30vw, (max-width: 30em) 100%, (max-width: 40em) 90vw'
+      imgInMemory.src = fullImage
+      imgInMemory.srcSet = srcSet
+      imgInMemory.sizes = sizes
+      imgInMemory.onload = () => {
+        imgRef.current.setAttribute('src', fullImage)
+        imgRef.current.setAttribute('srcSet', srcSet)
+        imgRef.current.setAttribute('sizes', sizes)
+        imgRef.current.classList.remove('is-loading')
+      }
+    },
+    [imgRef.current]
+  )
+
+  return imgRef
+}
