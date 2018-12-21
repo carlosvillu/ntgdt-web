634ef45932d4992c5081b3cc6c78ceda2d5d017d
Carlos Villuendas <carlosvillu@gmail.com>
2018-12-21 18:06:02 +0100

    feat(components): click in favorite works!

--- src/components/Icons/Favorite.js
+++ src/components/Icons/Favorite.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const SvgComponent = props => {
-  const {isFavorite, ...htmlProps} = props
+  const {isFavorite, onClick, ...htmlProps} = props
   return (
     <svg
       width="1em"
@@ -10,6 +10,7 @@ const SvgComponent = props => {
       viewBox="0 0 24 24"
       {...htmlProps}
       fill={isFavorite ? '#ff6335' : null}
+      onClick={onClick}
     >
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
@@ -18,6 +19,7 @@ const SvgComponent = props => {
 }
 
 SvgComponent.propTypes = {
+  onClick: PropTypes.func,
   isFavorite: PropTypes.bool
 }
 
--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import {useFavoritesFirebase} from '../../hooks/firebase'
+
 import Image from '../Image'
 import Favorite from '../Icons/Favorite'
 import GetApp from '../Icons/GetApp'
@@ -8,6 +10,7 @@ import Send from '../Icons/Send'
 
 const Item = ({item, style, onClick}) => {
   const {title, image, image_blur: blur} = item
+  const {isFavorite, callbackHandleClick} = useFavoritesFirebase(item)
   return (
     <div className="Item" style={style}>
       <h2 className="Item-title">{title}</h2>
@@ -15,7 +18,11 @@ const Item = ({item, style, onClick}) => {
         <Image blur={blur} src={image} alt={title} />
       </div>
       <div className="Item-icons">
-        <Favorite className="Item-icon" />
+        <Favorite
+          className="Item-icon"
+          isFavorite={isFavorite}
+          onClick={callbackHandleClick}
+        />
         <GetApp className="Item-icon" />
         <Send className="Item-icon" />
       </div>
