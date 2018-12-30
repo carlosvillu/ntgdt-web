6a03bd929ce51cef0f9e26903be3e9238efab0f9
Carlos Villuendas <carlosvillu@gmail.com>
2018-12-30 14:16:44 +0100

    feat(pages): add i18n

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
 import {useFirebaseRef} from '../../hooks/firebase'
 
+import Context from '../../context'
 import Grid from '../../components/Grid'
 
 const Home = () => {
+  const {i18n} = useContext(Context)
   const {loading, items = []} = useFirebaseRef('/entries')
   return (
     <div className="Home">
-      {!loading ? <Grid items={items} /> : <h1>Loading database</h1>}
+      {!loading ? <Grid items={items} /> : <h1>{i18n.t('HOME_LOADING')}</h1>}
     </div>
   )
 }
