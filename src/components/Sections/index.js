00550d33ee601bbf100338acaed1d4bcccb2ff0a
Carlos Villuendas <carlosvillu@gmail.com>
2018-12-30 14:16:23 +0100

    feat(components): add i18n

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Link from 'react-router/lib/Link'
 
-const Sections = () => (
-  <ul className="Sections">
-    <li className="Sections-section">
-      <Link className="Sections-link" to="/" activeClassName="is-selected">
-        <span className="Sections-literal">New</span>
-      </Link>
-    </li>
-    <li className="Sections-section">
-      <Link
-        className="Sections-link"
-        to="/favorites"
-        activeClassName="is-selected"
-      >
-        <span className="Sections-literal">Favorites</span>
-      </Link>
-    </li>
-  </ul>
-)
+import Context from '../../context'
+
+const Sections = () => {
+  const {i18n} = useContext(Context)
+  return (
+    <ul className="Sections">
+      <li className="Sections-section">
+        <Link className="Sections-link" to="/" activeClassName="is-selected">
+          <span className="Sections-literal">{i18n.t('SECTIONS_NEWS')}</span>
+        </Link>
+      </li>
+      <li className="Sections-section">
+        <Link
+          className="Sections-link"
+          to="/favorites"
+          activeClassName="is-selected"
+        >
+          <span className="Sections-literal">
+            {i18n.t('SECTIONS_FAVORITES')}
+          </span>
+        </Link>
+      </li>
+    </ul>
+  )
+}
 
 Sections.displayName = 'Sections'
 
