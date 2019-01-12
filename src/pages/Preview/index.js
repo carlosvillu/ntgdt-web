b18eb6e7967c30f4934b194326836ceebccafdd9
Carlos Villuendas <carlosvillu@gmail.com>
2019-01-12 19:28:09 +0100

    feat(pages): add preview page

--- src/pages/Preview/index.js
+++ src/pages/Preview/index.js
@@ -1,13 +1,57 @@
-import React from 'react'
+import React, {useState, useCallback} from 'react'
 import PropTypes from 'prop-types'
 
 import {useItemFirebase} from '../../hooks/firebase'
 import Item from '../../components/Item'
+import FullScreen from '../../components/FullScreen'
+import HeaderSeoItem from '../../components/HeaderSeoItem'
 
 const Preview = ({router}) => {
   const {loading, item} = useItemFirebase(router.params.id)
+  const [currentItem, setCurrentItem] = useState({})
+  const [isOpenImage, setIsOpenImage] = useState(false)
+  const handleCloseImage = useCallback(() => {
+    const event = new window.CustomEvent('tracker:event', {
+      detail: {
+        category: 'Item',
+        action: 'fullscreen',
+        label: 'close'
+      }
+    })
+    document.dispatchEvent(event)
+    setIsOpenImage(false)
+  })
   return (
-    <div className="Preview">{!loading && item && <Item item={item} />}</div>
+    <div className="Preview">
+      {!loading &&
+        item && (
+          <React.Fragment>
+            <HeaderSeoItem item={item} />
+            <Item
+              hiddenShare
+              item={item}
+              onClick={() => {
+                const event = new window.CustomEvent('tracker:event', {
+                  detail: {
+                    category: 'Item',
+                    action: 'fullscreen',
+                    label: 'open'
+                  }
+                })
+                document.dispatchEvent(event)
+                setCurrentItem(item)
+                setIsOpenImage(true)
+              }}
+            />
+            <FullScreen
+              video={currentItem.video}
+              image={currentItem.image}
+              isOpen={isOpenImage}
+              onClose={handleCloseImage}
+            />
+          </React.Fragment>
+        )}
+    </div>
   )
 }
 
