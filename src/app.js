2c83a8abcd17052e8942c05bd308ebe0528e9870
Carlos Villuendas <carlosvillu@gmail.com>
2019-05-25 13:42:16 +0200

    feat(Root): instanciate FB in entry point and add metrics

--- src/app.js
+++ src/app.js
@@ -12,6 +12,37 @@ import Perfume from 'perfume.js'
 import {register} from '@s-ui/bundler/registerServiceWorker'
 
 import './styles/index.scss'
+import * as firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/performance'
+
+const configPRO = {
+  apiKey: 'AIzaSyCeUsBwp1gzKDwQqorrri7nRlqr_QXtg1g',
+  authDomain: 'no-tengo-ganas-de-trabajar.firebaseapp.com',
+  databaseURL: 'https://no-tengo-ganas-de-trabajar.firebaseio.com',
+  projectId: 'no-tengo-ganas-de-trabajar',
+  storageBucket: 'no-tengo-ganas-de-trabajar.appspot.com',
+  messagingSenderId: '1069878588859',
+  appId: '1:1069878588859:web:cf2c3c6b73d88a32'
+}
+
+const configDEV = {
+  apiKey: 'AIzaSyABQ1Mkhh37tNfG61eAl46lR0SvdAvZKN0',
+  authDomain: 'no-tengo-ganas-de-trabajar-dev.firebaseapp.com',
+  databaseURL: 'https://no-tengo-ganas-de-trabajar-dev.firebaseio.com',
+  projectId: 'no-tengo-ganas-de-trabajar-dev',
+  storageBucket: 'no-tengo-ganas-de-trabajar-dev.appspot.com',
+  messagingSenderId: '554532477116',
+  appId: '1:554532477116:web:253f68c83b91f6ce'
+}
+
+firebase.initializeApp(
+  process.env.STAGE === 'production' ? configPRO : configDEV
+)
+const perf = firebase.performance()
+
+window.firebase = firebase
+window.perfFB = perf
 
 const perfume = new Perfume({
   // Metrics
@@ -25,13 +56,16 @@ const perfume = new Perfume({
   }
 })
 
+const traceRender = perf.trace('render')
 perfume.start('render')
+traceRender.start()
 ReactDOM.render(
   <Context.Provider value={{i18n}}>
     <Router routes={routes} history={browserHistory} />
   </Context.Provider>,
   document.getElementById('⚛️')
 )
+traceRender.stop()
 perfume.sendTiming('render', perfume.end('render'))
 
 document.addEventListener('tracker:event', evt => {
