0ce632b35e8bb15d15a01a9f6ceaa32f49decf42
Carlos Villuendas <carlosvillu@gmail.com>
2019-05-25 13:41:46 +0200

    refactor(hooks): remove instanciate FB

--- src/hooks/firebase.js
+++ src/hooks/firebase.js
@@ -1,30 +1,6 @@
+/* global firebase */
 import {useState, useEffect, useCallback} from 'react'
 import {get, set} from 'idb-keyval'
-
-import * as firebase from 'firebase/app'
-import 'firebase/database'
-
-const configPRO = {
-  apiKey: 'AIzaSyCeUsBwp1gzKDwQqorrri7nRlqr_QXtg1g',
-  authDomain: 'no-tengo-ganas-de-trabajar.firebaseapp.com',
-  databaseURL: 'https://no-tengo-ganas-de-trabajar.firebaseio.com',
-  projectId: 'no-tengo-ganas-de-trabajar',
-  storageBucket: 'no-tengo-ganas-de-trabajar.appspot.com',
-  messagingSenderId: '1069878588859'
-}
-
-const configDEV = {
-  apiKey: 'AIzaSyABQ1Mkhh37tNfG61eAl46lR0SvdAvZKN0',
-  authDomain: 'no-tengo-ganas-de-trabajar-dev.firebaseapp.com',
-  databaseURL: 'https://no-tengo-ganas-de-trabajar-dev.firebaseio.com',
-  projectId: 'no-tengo-ganas-de-trabajar-dev',
-  storageBucket: 'no-tengo-ganas-de-trabajar-dev.appspot.com',
-  messagingSenderId: '554532477116'
-}
-
-firebase.initializeApp(
-  process.env.STAGE === 'production' ? configPRO : configDEV
-)
 
 const NOT_FOUND = -1
 const MAX_ITEMS = 100
