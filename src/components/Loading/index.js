2d7c28ae7658e5ab63891934bf882030a5bd4040
Carlos Villuendas <carlosvillu@gmail.com>
2020-04-14 07:56:43 +0200

    feat(components): dont show the spinner the frist 145ms

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import Settings from '../Icons/Settings'
 
-const Loading = () => (
-  <div className="Loading">
-    <Settings />
-  </div>
-)
+const Loading = () => {
+  const {showSpinner, setShowSpinner} = useState(false)
+  useEffect(() => {
+    setTimeout(() => setShowSpinner(true), 145)
+  })
+
+  return <div className="Loading">{showSpinner && <Settings />}</div>
+}
 
 export default Loading
