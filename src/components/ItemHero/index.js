5e192845f94ccda92fcee90247d4cfdae6c19de5
Gabriel Salas <ggsalas@gmail.com>
2020-04-17 20:40:15 +0200

    feat(components): slide navigation

--- src/components/ItemHero/index.js
+++ src/components/ItemHero/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PropTypes from 'prop-types'
+import Hammer from 'react-hammerjs'
 
+import RRContext from '@s-ui/react-router/lib/ReactRouterContext'
 import {useItemFavoriteFirebase} from '../../hooks/firebase'
 import Share from '../Share'
 import Image from '../Image'
@@ -12,61 +14,72 @@ const ItemHero = ({
   item,
   videoHeight,
   heroWidth,
-  maxWithForLongVerticalImages
+  maxWithForLongVerticalImages,
+  nextItemId
 }) => {
+  const {router} = useContext(RRContext)
   const {title, site, image, video, image_blur: blur} = item
-  const {isFavorite, callbackHandleClick} = useItemFavoriteFirebase(item)
+  const {isFavorite, callbackHandleClick} = useItemFavoriteFirebase(item.id)
   const height = (
     (item.height * maxWithForLongVerticalImages) /
     item.width
   ).toFixed()
 
   return (
-    <div className="ItemHero">
-      {/* TODO: get image max-width 1024 px */}
-      {image && (
-        <Image
-          width={maxWithForLongVerticalImages}
-          height={height}
-          image_blur={blur}
-          src={image}
-          alt={title}
-          style={{width: `${maxWithForLongVerticalImages}px`}}
-        />
-      )}
-      {video && <Video {...video} width={heroWidth} height={videoHeight} />}
-
-      <div className="ItemHero-meta">
-        <div className="ItemHero-description">
-          <h3 className="ItemHero-site">{site}</h3>
-          {title !== site && <h2 className="ItemHero-title">{title}</h2>}
-        </div>
-
-        <div className="ItemHero-actions">
-          <Share
-            item={item}
-            href={`whatsapp://send?text=${encodeURIComponent(
-              `https://${process.env.HOST}/preview?id=${item.id}`
-            )}`}
-            aria-label="Send"
-            onClick={() =>
-              document.dispatchEvent(
-                new window.CustomEvent('tracker:event', {
-                  detail: {category: 'Action', action: 'shared'}
-                })
-              )
-            }
-          >
-            <Send className="ItemHero-icon" />
-          </Share>
-
-          <Favorite
-            className={`ItemHero-icon ${isFavorite ? 'is-favorite' : ''}`}
-            onClick={callbackHandleClick}
+    <Hammer
+      onSwipe={e => {
+        if (e.direction === 4) return router.goBack()
+        if (e.direction === 2)
+          return router.push({pathname: '/meme', query: {id: nextItemId}})
+      }}
+      onDoubleTap={callbackHandleClick}
+    >
+      <div className="ItemHero">
+        {/* TODO: get image max-width 1024 px */}
+        {image && (
+          <Image
+            width={maxWithForLongVerticalImages}
+            height={height}
+            image_blur={blur}
+            src={image}
+            alt={title}
+            style={{width: `${maxWithForLongVerticalImages}px`}}
           />
+        )}
+        {video && <Video {...video} width={heroWidth} height={videoHeight} />}
+
+        <div className="ItemHero-meta">
+          <div className="ItemHero-description">
+            <h3 className="ItemHero-site">{site}</h3>
+            {title !== site && <h2 className="ItemHero-title">{title}</h2>}
+          </div>
+
+          <div className="ItemHero-actions">
+            <Share
+              item={item}
+              href={`whatsapp://send?text=${encodeURIComponent(
+                `https://${process.env.HOST}/preview?id=${item.id}`
+              )}`}
+              aria-label="Send"
+              onClick={() =>
+                document.dispatchEvent(
+                  new window.CustomEvent('tracker:event', {
+                    detail: {category: 'Action', action: 'shared'}
+                  })
+                )
+              }
+            >
+              <Send className="ItemHero-icon" />
+            </Share>
+
+            <Favorite
+              className={`ItemHero-icon ${isFavorite ? 'is-favorite' : ''}`}
+              onClick={callbackHandleClick}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </Hammer>
   )
 }
 
@@ -87,7 +100,8 @@ ItemHero.propTypes = {
   }),
   videoHeight: PropTypes.number,
   heroWidth: PropTypes.number,
-  maxWithForLongVerticalImages: PropTypes.number
+  maxWithForLongVerticalImages: PropTypes.number,
+  nextItemId: PropTypes.string
 }
 
 export default ItemHero
