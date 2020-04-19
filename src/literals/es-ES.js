78803c2608d51426ee71a9fcddc657664ced2ecc
Carlos Villuendas <carlosvillu@gmail.com>
2020-04-19 15:28:57 +0200

    feat(literals): add new menu literal

--- src/literals/es-ES.js
+++ src/literals/es-ES.js
@@ -1,8 +1,9 @@
 export default {
-  INSTALL_PWA_EXPLAINER: 'Inagotable lista de memes para cuando No Tienes Ganas de Trabajar', // eslint-disable-line
+  HEADER_SWITCH_THEME: 'Cambiar tema',
+  INSTALL_PWA_CANCEL_BUTTON: 'Cancelar',
   INSTALL_PWA_DESCRIPTION_HEADER: 'Descripción',
+  INSTALL_PWA_EXPLAINER: 'Inagotable lista de memes para cuando No Tienes Ganas de Trabajar', // eslint-disable-line
   INSTALL_PWA_FEATURE_HEADER: 'Principales características',
   INSTALL_PWA_INSTALL_BUTTON: 'Instalar',
-  INSTALL_PWA_CANCEL_BUTTON: 'Cancelar',
   INSTALL_PWA_IOS_INSTALL: 'Click en el botón compartir y entonces añadir a Patalla de Inicio' // eslint-disable-line
 }
